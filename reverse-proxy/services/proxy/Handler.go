7d001f9bfa1aa7a4bb7b84dc775ef140dabdcc9a
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2026-01-23 00:42:28 +0100

    Proxy side

---
+++ reverse-proxy/services/proxy/Handler.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	// "flag"
+)
+
+
+func HandleGet(w http.ResponseWriter, _ *http.Request){
+	fmt.Printf("helooooooo")
+
+
+	fmt.Println("oplaaa")
+
+	jsonResp , err := json.Marshal([]string{"awdii"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonResp)
+}
+
+func HandlePost(w http.ResponseWriter, r *http.Request){
+
+}
+
+func Handler(w http.ResponseWriter, r *http.Request){
+	switch r.Method {
+		case http.MethodGet:
+			HandleGet(w,r)
+		case http.MethodPost:
+			HandlePost(w,r)
+		default:
+			fmt.Print("Oops")
+	}
+}
+
+func main(){
+	http.HandleFunc("/status",Handler)
+	// http.HandleFunc("/backends",Handler)
+	http.ListenAndServe(":8081",nil)
+}
