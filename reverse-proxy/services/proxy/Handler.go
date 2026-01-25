b9b2361f153cdcc60f2a7ce4c8655cdbc56de240
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2026-01-25 23:23:56 +0100

    Clean code, select backend in handler and return 503 when none are available

--- reverse-proxy/services/proxy/Handler.go
+++ reverse-proxy/services/proxy/Handler.go
@@ -1,60 +1,39 @@
 package proxy
 
 import (
-	// "encoding/json"
+	"context"
 	"fmt"
-	// "io"
 	"net/http"
 	"net/http/httputil"
 	"reverse-proxy/services/models"
-	// "flag"
 )
 
-// var channel = make(chan []byte, 1)
-
-// func HandleGet(w http.ResponseWriter, _ *http.Request){
-
-// 	fmt.Printf("helooooooo")
-
-// 	fmt.Println("oplaaa")
-
-// 	jsonResp , err := json.Marshal([]string{"hhh"})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	channel <- jsonResp
-// 	// w.Write(jsonResp)
-// 	w.Write(<- channel)
-// }
-
-// func HandlePost(w http.ResponseWriter, r *http.Request){
-
-// }
-
-// func Handler(w http.ResponseWriter, r *http.Request){
-// 	switch r.Method {
-// 		case http.MethodGet:
-// 			HandleGet(w,r)
-// 		case http.MethodPost:
-// 			HandlePost(w,r)
-// 		default:
-// 			fmt.Print("Oops")
-// 	}
-// }
-
-type Handler struct{
+type Handler struct {
 	proxy *httputil.ReverseProxy
+	pool *models.ServerPool
 }
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request){  
-	h.proxy.ServeHTTP(w,r)
+	backend := h.pool.GetNextValidPeer()
+	if backend == nil {
+		http.Error(
+			w,
+			fmt.Sprintf("%d Service Unavailable", http.StatusServiceUnavailable),
+			http.StatusServiceUnavailable,
+		)
+		return
+	}
+
+	ctx := context.WithValue(r.Context(),"backend",backend)
+
+	h.proxy.ServeHTTP(w,r.WithContext(ctx))
 }
 
 
 
 func StartProxy(proxy models.ProxyConfig) {
 
-	fmt.Println("llllollllllll")
+	fmt.Println("Proxy starts! ")
 
 
 	server_pool := models.ServerPool {
@@ -64,54 +43,22 @@ func StartProxy(proxy models.ProxyConfig) {
 
 	
 	director := func (req *http.Request){
-		selected_backend := server_pool.GetNextValidPeer()
+		selected_backend := req.Context().Value("backend").(*models.Backend)
 
 		req.URL.Scheme = selected_backend.URL.Scheme
 		req.URL.Host = selected_backend.URL.Host
 		
-		fmt.Println("Url of the selected active backend: ",selected_backend.URL.String()) 
-
+		fmt.Println("Forwarding to:", selected_backend.URL.String())
 	}
 
 	reverse_proxy := &httputil.ReverseProxy{Director: director}
-	handler := Handler{proxy: reverse_proxy}
+
+	handler := Handler{
+		proxy: reverse_proxy,
+		pool: &server_pool,
+	}
 
 
 	http.Handle("/api", handler)
 	http.ListenAndServe(fmt.Sprintf(":%d",proxy.Port),nil)	
-
-// go func (){
-// 	http.HandleFunc("/status",Handler)
-// 	http.ListenAndServe(fmt.Sprintf(":%d",proxy.Admin_port),nil)
-// }()
-
-
-	// http.ListenAndServe(fmt.Sprintf(":%d",proxy.Port),nil)
 }
-
-
-
-
-
-
-// SOME OLD STUFF 
-// func(w http.ResponseWriter, r *http.Request) {
-
-				
-// 					resp, err := http.Get(selected_backend.URL.String())
-// 					if err != nil {
-// 						log.Fatal(err)
-// 					}
-
-// 					defer resp.Body.Close()
-
-// 					bytes, err := io.ReadAll(resp.Body)
-
-// 					result := []string{}
-
-// 					_  = json.Unmarshal(bytes,&result)
-// 					if err != nil {
-// 						log.Fatal(err)}
-
-// 				w.Write(bytes)
-// 			}
