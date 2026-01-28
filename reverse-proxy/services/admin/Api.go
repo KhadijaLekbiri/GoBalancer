2f63f11f0f54b13738cb7c9f5c5ca76252a0317e
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2026-01-28 23:55:23 +0100

    Return the state of all backends

--- reverse-proxy/services/admin/Api.go
+++ reverse-proxy/services/admin/Api.go
@@ -3,11 +3,11 @@ package admin
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"reverse-proxy/services/models"
-
-	// "flag"
 )
 
 
@@ -19,21 +19,75 @@ func NewAPI(pool *models.ServerPool) *API {
 	return &API{Pool: pool}
 }
 
+type StateResponse struct {
+	Total_backends int
+	Active_backends int
+	Backends []BackendResponse
+}
 
+type BackendResponse struct {
+	URL          string `json:"url"`
+	Alive        bool     `json:"alive"`
+	CurrentConnections int64    `json:"current_connections"`
+}
 
 func (api *API) HandleGet(w http.ResponseWriter, _ *http.Request){
 	fmt.Printf("helooooooo")
 
 	backends := api.Pool.Backends
 
-	jsonResp , err := json.MarshalIndent(backends,"","	")
+	state := StateResponse {
+		Total_backends: len(backends),
+		Backends: make([]BackendResponse, 0, len(backends)),
+	}
+
+	for _, backend := range backends {
+		alive := backend.IsAlive()
+		if alive {
+			state.Active_backends++
+		}
+
+		state.Backends = append(state.Backends, BackendResponse {
+			URL: backend.URL.String(),
+			Alive: alive,
+			CurrentConnections: backend.CurrentConns,
+		})
+	}
+
+	jsonResp , err := json.MarshalIndent(state,"","	")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	w.Header().Set("Content-Type","application/json")
 	w.Write(jsonResp)
 }
 
-func HandlePost(w http.ResponseWriter, r *http.Request){
+func (api *API) HandlePost(w http.ResponseWriter, r *http.Request) {
+
+	new_url := struct {
+		Url string  `json:"url"`}{
+			Url: "",
+		}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal([]byte(body), &new_url)
+
+	url, _ := url.ParseRequestURI(new_url.Url)
+
+	newBackend := models.Backend{
+		URL: url,
+		Alive: false,
+		CurrentConns: 0,
+	}
+
+	api.Pool.AddBackend(&newBackend)
+
+	w.WriteHeader(http.StatusCreated)
 
 }
 
@@ -43,7 +97,7 @@ func (api *API) Handler(w http.ResponseWriter, r *http.Request){
 		case http.MethodGet:
 			api.HandleGet(w,r)
 		case http.MethodPost:
-				HandlePost(w,r)
+			api.HandlePost(w,r)
 		default:
 			fmt.Print("Oops")
 	}
@@ -51,31 +105,9 @@ func (api *API) Handler(w http.ResponseWriter, r *http.Request){
 
 func CheckStatus(pool *models.ServerPool) {
 	my_api :=  NewAPI(pool)
-
+		
+	http.HandleFunc("/backends",my_api.Handler)
 	http.HandleFunc("/status",my_api.Handler)
-	// http.HandleFunc("/backends",Handler)
+
 	http.ListenAndServe(":8081",nil)
-}
-
-
-// SOME OLD STUFF 
-// func(w http.ResponseWriter, r *http.Request) {
-
-				
-// 					resp, err := http.Get(selected_backend.URL.String())
-// 					if err != nil {
-// 						log.Fatal(err)
-// 					}
-
-// 					defer resp.Body.Close()
-
-// 					bytes, err := io.ReadAll(resp.Body)
-
-// 					result := []string{}
-
-// 					_  = json.Unmarshal(bytes,&result)
-// 					if err != nil {
-// 						log.Fatal(err)}
-
-// 				w.Write(bytes)
-// 			}
+}	
