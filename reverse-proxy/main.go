d2b3b9d4604e8c2cd0a4de85681f21fd92b04f7b
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2026-02-28 22:10:50 +0000

    refactor: remove Healthcheck and clean code

--- reverse-proxy/main.go
+++ reverse-proxy/main.go
@@ -10,6 +10,7 @@ import (
 	"reverse-proxy/services/admin"
 	"reverse-proxy/services/models"
 	"reverse-proxy/services/proxy"
+	"reverse-proxy/services/health"
 	"strings"
 	"sync"
 	"time"
@@ -17,41 +18,6 @@ import (
 
 
 
-
-func Healthcheck(server_pool *models.ServerPool, timeout time.Duration) {
-	// here we could assume that we go through the backends and ping them
-
-	server_pool.Mux.RLock()
-
-	backends := make([]*models.Backend, len(server_pool.Backends))
-	copy(backends,server_pool.Backends)
-
-	server_pool.Mux.RUnlock()
-		
-	var wg sync.WaitGroup
-
-	for i, backend := range backends {
-
-		wg.Add(1)
-
-		go func (b *models.Backend) {
-			defer wg.Done()
-
-			client := &http.Client{Timeout: timeout}
-			resp, err :=  client.Get(b.URL.String())
-			
-			alive := err == nil && resp.StatusCode >= 200 && resp.StatusCode < 400
-			fmt.Println("Backend",i, "is: ",alive)
-			b.SetAlive(alive)
-			
-			if err == nil {
-				resp.Body.Close()
-			}
-			
-		}(backend)
-	}
-	wg.Wait()
-}
 
 func main() {
 	var wg sync.WaitGroup
@@ -64,7 +30,6 @@ func main() {
 		fmt.Print("No Config file given")
 		return
 	}
-	fmt.Println(*result)
 
 	data, _ := os.ReadFile(*result)
 
@@ -77,9 +42,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}				
-
-	fmt.Println(initial_proxy.Strategy)
-
 
 	server_pool := models.ServerPool {
 			Backends: models.Backends(),
@@ -101,7 +63,7 @@ func main() {
 		ticker := time.NewTicker(initial_proxy.HealthCheckFreq)
 
 		for range ticker.C {
-			Healthcheck(&server_pool,initial_proxy.Timeout)
+			health.Healthcheck(&server_pool,initial_proxy.Timeout)
 		}
 	}()
 	
@@ -136,10 +98,6 @@ func main() {
 		}
 	}()
 	wg.Wait()
-
-
-
-	fmt.Println("hani")
 	
 
 }
